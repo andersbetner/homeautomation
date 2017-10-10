adc43d48f0f4253ff4affcd22e373f1913941d92
Anders Betnér <anders.betner@gmail.com>
2017-10-10 18:36:08 +0200

    switch to mqttagent

--- ica/ica.go
+++ ica/ica.go
@@ -11,21 +11,22 @@ import (
 	"io/ioutil"
 	"net/http"
 	"os"
+	"os/signal"
 	"strconv"
 	"time"
 
 	log "github.com/Sirupsen/logrus"
 	"github.com/andersbetner/homeautomation/util"
+	ag "github.com/andersbetner/mqttagent"
+	mqtt "github.com/eclipse/paho.mqtt.golang"
 	"github.com/prometheus/client_golang/prometheus"
-	"gobot.io/x/gobot"
-	"gobot.io/x/gobot/platforms/mqtt"
 )
 
 var (
-	mqttAdaptor     *mqtt.Adaptor
 	mqttHost        string
 	icaUser         string
 	icaPassword     string
+	agent           *ag.Agent
 	updateInterval  int // Minutes default = 30
 	promUpdateCount = prometheus.NewCounterVec(prometheus.CounterOpts{
 		Name: "ab_update_count",
@@ -40,6 +41,75 @@ var (
 
 type accountInfo struct {
 	AvailableAmount float64
+}
+
+// update gets the latest account funds from ica.se
+func update() {
+	client := &http.Client{
+		CheckRedirect: nil,
+		Timeout:       time.Second * 10,
+	}
+	request, _ := http.NewRequest("GET", "https://api.ica.se/api/login/", nil)
+	request.SetBasicAuth(icaUser, icaPassword)
+	resp, err := client.Do(request)
+	if err != nil {
+		promUpdateCount.WithLabelValues("500").Inc()
+		log.WithField("error", err).Error("Error in basic auth")
+		return
+	}
+	defer resp.Body.Close()
+
+	authTicket := resp.Header.Get("AuthenticationTicket")
+	request, _ = http.NewRequest("GET", "https://api.ica.se/api/user/minasidor/", nil)
+	request.Header.Add("AuthenticationTicket", authTicket)
+	resp, err = client.Do(request)
+	if err != nil {
+		promUpdateCount.WithLabelValues("500").Inc()
+		log.WithField("error", err).Error("Error in authticket")
+
+		return
+	}
+	defer resp.Body.Close()
+	jsonBlob, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		promUpdateCount.WithLabelValues("500").Inc()
+		log.WithField("error", err).Error("Error parsing response body")
+
+		return
+	}
+	ica := accountInfo{}
+	err = json.Unmarshal(jsonBlob, &ica)
+	if err != nil {
+		promUpdateCount.WithLabelValues("500").Inc()
+		log.WithField("error", err).Error("Error unmarshal json")
+		return
+	}
+
+	err = agent.Publish("ica/availableamount", true, strconv.Itoa(int(ica.AvailableAmount)))
+	if err != nil {
+		promUpdateCount.WithLabelValues("500").Inc()
+		log.WithField("error", "mqtt").Error("Error publishing ica/availableamount")
+
+		return
+	}
+	err = agent.Publish("ica/all", true, string(jsonBlob))
+	if err != nil {
+		promUpdateCount.WithLabelValues("500").Inc()
+		log.WithField("error", err).Error("Error publishing ica/all")
+
+		return
+	}
+
+	promUpdateCount.WithLabelValues("200").Inc()
+	promUpdateTimestamp.SetToCurrentTime()
+
+}
+
+// updateHandler acts on mqtt messages to ica/update
+func updateHandler(client mqtt.Client, msg mqtt.Message) {
+	command := string(msg.Payload())
+	log.WithFields(log.Fields{"command": command}).Debug("Update requested through mqtt")
+	update()
 }
 
 func init() {
@@ -63,95 +133,46 @@ func init() {
 		exit = true
 	}
 	interval, _ := os.LookupEnv("ICA_UPDATE_INTERVAL")
-	if interval != "" {
-		var err error
-		updateInterval, err = strconv.Atoi(interval)
-		if err != nil || updateInterval < 1 {
-			os.Stderr.WriteString("env ICA_UPDATE_INTERVAL must be integer > 0\n")
-			exit = true
-		}
+	if interval == "" {
+		os.Stderr.WriteString("env ICA_UPDATE_INTERVAL missing\n")
+		exit = true
 	}
+	var err error
+	updateInterval, err = strconv.Atoi(interval)
+	if err != nil || updateInterval < 1 {
+		os.Stderr.WriteString("env ICA_UPDATE_INTERVAL must be integer > 0\n")
+		exit = true
+	}
+
 	if exit {
 		os.Exit(1)
 	}
 }
 
-// update gets the latest account funds from ica.se
-func update() {
-	client := &http.Client{
-		CheckRedirect: nil,
-		Timeout:       time.Second * 10,
-	}
-	request, _ := http.NewRequest("GET", "https://api.ica.se/api/login/", nil)
-	request.SetBasicAuth(icaUser, icaPassword)
-	resp, err := client.Do(request)
-	if err != nil {
-		promUpdateCount.WithLabelValues("500").Inc()
-		log.WithField("error", "basicauth").Error("Error in basic auth")
-		return
-	}
-	defer resp.Body.Close()
-
-	authTicket := resp.Header.Get("AuthenticationTicket")
-	request, _ = http.NewRequest("GET", "https://api.ica.se/api/user/minasidor/", nil)
-	request.Header.Add("AuthenticationTicket", authTicket)
-	resp, err = client.Do(request)
-	if err != nil {
-		promUpdateCount.WithLabelValues("500").Inc()
-		log.WithField("error", "authticket").Error("Error in authticket")
-
-		return
-	}
-	defer resp.Body.Close()
-	jsonBlob, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		promUpdateCount.WithLabelValues("500").Inc()
-		log.WithField("error", "body").Error("Error parsing response body")
-
-		return
-	}
-	ica := accountInfo{}
-	err = json.Unmarshal(jsonBlob, &ica)
-	if err != nil {
-		promUpdateCount.WithLabelValues("500").Inc()
-		log.WithField("error", "unmarshal").Error("Error unmarshal json")
-		return
-	}
-
-	if !mqttAdaptor.Publish("ica/availableamount", []byte(strconv.Itoa(int(ica.AvailableAmount)))) ||
-		!mqttAdaptor.Publish("ica/all", []byte(string(jsonBlob))) {
-		promUpdateCount.WithLabelValues("500").Inc()
-		log.WithField("error", "mqtt").Error("Error publishing mqtt")
-
-		return
-	}
-
-	promUpdateCount.WithLabelValues("200").Inc()
-	promUpdateTimestamp.SetToCurrentTime()
-
-}
-
-// updateHandler acts on mqtt messages to ica/update
-func updateHandler(msg mqtt.Message) {
-	command := string(msg.Payload())
-	log.WithFields(log.Fields{"command": command}).Debug("Update requested through mqtt")
-	update()
-}
-
 func main() {
 	prometheusMux := http.NewServeMux()
 	prometheusMux.Handle("/metrics", prometheus.Handler())
-	go util.Webserver("Prometheus", ":9100", prometheusMux)
+	go util.Webserver("prometheus", ":9100", prometheusMux)
 
-	mqttAdaptor = mqtt.NewAdaptor(mqttHost, "ica")
-	work := func() {
-		update()
-		mqttAdaptor.On("ica/update", updateHandler)
-		gobot.Every(time.Duration(updateInterval)*time.Minute, update)
+	agent = ag.NewAgent(mqttHost, "ica")
+	err := agent.Connect()
+	if err != nil {
+		log.WithField("error", err).Error("Can't connect to mqtt server")
+		os.Exit(1)
 	}
-	robot := gobot.NewRobot("ica",
-		[]gobot.Connection{mqttAdaptor},
-		work,
-	)
-	robot.Start()
+	agent.Subscribe("ica/update", updateHandler)
+	go func() {
+		done := make(chan os.Signal)
+		signal.Notify(done, os.Interrupt)
+		<-done
+		log.Info("Shutting down ica")
+		time.Sleep(2 * time.Second)
+		os.Exit(0)
+	}()
+
+	for !agent.IsTerminated() {
+		update()
+		time.Sleep(time.Duration(updateInterval) * time.Minute)
+	}
+
 }
